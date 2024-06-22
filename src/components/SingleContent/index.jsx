8de19d2d2070f761bf668fde85cc13ca35d0d46d
Gilles Lucien <gpeltier2@gmail.com>
2024-06-22 19:21:55 +0200

    single content layout and start of the gallery component

--- src/components/SingleContent/index.jsx
+++ src/components/SingleContent/index.jsx
@@ -24,8 +24,40 @@ export default function SingleContent(project) {
           src={imageMap.img1}
           alt={project.title + project.id + imageMap.img1}
         />
-        <div className="singleContent__container__txt">
-          <p>{project.description}</p>
+        <div className="singleContent__container__txt--section1">
+          <h2>
+            {project.section1.title}
+            <span className="blueSpan">{project.section1.subtitle}</span>
+          </h2>
+          <p className="twoColumns">{project.section1.description}</p>
+        </div>
+        <img
+          src={imageMap.img2}
+          alt={project.title + project.id + imageMap.img2}
+        />
+        <div className="displayRow">
+          <div className="singleContent__container__txt--section2">
+            <h2>
+              {project.section2.title}
+              <span className="blueSpan">{project.section2.subtitle}</span>
+            </h2>
+            <p>{project.section2.description}</p>
+          </div>
+          <img
+            src={imageMap.img3}
+            alt={project.title + project.id + imageMap.img3}
+          />
+        </div>
+        <div className="singleContent__container__gallery">
+          {Array.from({ length: project.imageNumber - 3 }, (_, index) => (
+            <img
+              key={index}
+              src={imageMap[`img${index + 4}`]} // Commence à img4 car vous soustrayez 3
+              alt={`${project.title}${project.id}${
+                imageMap[`img${index + 4}`]
+              }`}
+            />
+          ))}
         </div>
       </div>
     </section>
