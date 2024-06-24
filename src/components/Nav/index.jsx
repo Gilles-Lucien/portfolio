bb8f347e987ffe8786584797301d3665929c1a7d
Gilles Lucien <gpeltier2@gmail.com>
2024-06-24 18:40:34 +0200

    chore: Update Nav styles for responsive design

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Button from "../Button";
 import logo from "../../assets/images/logo_black.png";
+import Close from "../../assets/svg/closeDark.svg";
 import { NavLink, Link } from "react-router-dom";
 import "../../utils/variables/fontStyles.css";
 import "./styles.css";
@@ -9,38 +10,40 @@ export default function Nav() {
   const navMobileRef = useRef(null);
   const logoContainerRef = useRef(null);
   const navBarRef = useRef(null);
+  const LogoClose = useRef(null);
 
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [srcValue, setSrcValue] = useState(logo);
 
-  // const handleLogoClick = () => {
-  //   if (navBarRef.current && logoContainerRef.current) {
-  //     const navWidth = navBarRef.current.offsetWidth;
-  //     const navBarWidth = navBarRef.current.offsetWidth;
-  //     navBarRef.current.style.transform = `translateX(${navBarWidth}px)`;
-  //     logoContainerRef.current.style.transform = `translateX(${navWidth - 50}px)`;
-  //   }
-  // };
 
   const handleLogoClick = () => {
     if (isNavOpen) {
-        setIsNavOpen(false);
-        navMobileRef.current.removeAttribute("id");
-        setTimeout(() => {
-          if (navMobileRef.current) {
-              navMobileRef.current.setAttribute("id", "navClose");
-          }
+      setIsNavOpen(false);
+      navMobileRef.current.removeAttribute("id");
+      setTimeout(() => {
+        if (navMobileRef.current) {
+          navMobileRef.current.setAttribute("id", "navClose");
+        }
       }, 300);
     } else {
-        setIsNavOpen(true);
-        navMobileRef.current.setAttribute("id", "navOpen");
+      setIsNavOpen(true);
+      navMobileRef.current.setAttribute("id", "navOpen");
     }
-};
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSrcValue(isNavOpen ? Close : logo);
+    }, 200); // Attendre 200 millisecondes avant de changer
+
+    return () => clearTimeout(timer);
+  }, [isNavOpen]);
 
   return (
     <>
       <nav id="navDesktop">
         <div id="nav" className="navContainer">
-          <Link className="navContainer__logoContainer" to="/">
+          <Link className="navContainer__logoContainer" to="/" >
             <img src={logo} alt="Logo" />
             <p id="navName">
               Lucien Peltier <strong className="fira-mono-bold">|</strong>
@@ -70,7 +73,7 @@ export default function Nav() {
         </div>
       </nav>
 
-      <nav id="navMobile" >
+      <nav id="navMobile">
         <div id="nav" ref={navMobileRef} className="mobileNavContainer">
           <div
             ref={logoContainerRef}
@@ -78,13 +81,14 @@ export default function Nav() {
             onClick={handleLogoClick}
           >
             <img
-              src={logo}
+              src={srcValue}
               alt="Logo"
               className="mobileNavContainer__logoButton--logo"
+              ref={LogoClose}
             />
           </div>
           <div className="mobileNavContainer__navBar" ref={navBarRef}>
-            <Link className="mobileNavContainer__navBar__name" to="/">
+            <Link className="mobileNavContainer__navBar__name" to="/" onClick={handleLogoClick}>
               <p>
                 Lucien Peltier <br />
                 <strong className="fira-mono-bold">
@@ -95,18 +99,18 @@ export default function Nav() {
             </Link>
             <ul>
               <li>
-                <NavLink to="/projects" className="navLink">
+                <NavLink to="/projects" className="navLink" onClick={handleLogoClick}>
                   <p>Projects</p>
                 </NavLink>
               </li>
               <li>
-                <NavLink to="/aboutme" className="navLink">
+                <NavLink to="/aboutme" className="navLink" onClick={handleLogoClick}>
                   <p>About me</p>
                 </NavLink>
               </li>
               <li>
-                <Link to="/contact">
-                  <Button className="contactButton">Contact</Button>
+                <Link to="/contact" onClick={handleLogoClick}>
+                  <Button className="contactButton" >Contact</Button>
                 </Link>
               </li>
             </ul>
