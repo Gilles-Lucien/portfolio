56264347dd9b1c3008c4dc999807415285c9a8af
Gilles Lucien <gpeltier2@gmail.com>
2024-06-24 16:49:10 +0200

    chore: Update Hero and Nav styles for responsive design

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Button from "../Button";
 import logo from "../../assets/images/logo_black.png";
 import { NavLink, Link } from "react-router-dom";
@@ -6,41 +6,94 @@ import "../../utils/variables/fontStyles.css";
 import "./styles.css";
 
 export default function Nav() {
+  const navMobileRef = useRef(null);
+  const logoContainerRef = useRef(null);
+  const navBarRef = useRef(null);
+
+  // const handleLogoClick = () => {
+  //   if (navBarRef.current && logoContainerRef.current) {
+  //     const navWidth = navBarRef.current.offsetWidth;
+  //     const navBarWidth = navBarRef.current.offsetWidth;
+  //     navBarRef.current.style.transform = `translateX(${navBarWidth}px)`;
+  //     logoContainerRef.current.style.transform = `translateX(${navWidth - 50}px)`;
+  //   }
+  // };
+
+
+
   return (
-    <nav>
-      <div id="nav" className="navContainer">
-        <Link className="navContainer__logoContainer" to="/">
-          <img src={logo} alt="Logo" />
-          <p id="navName">
-            Lucien Peltier <strong className="fira-mono-bold">|</strong>
-          </p>
-          <p className="fira-mono-bold" id="navJob">
-            UX/UI/Graphic designer
-            <br />& Front-end developer
-          </p>
-        </Link>
-        <ul>
-          <li>
-            <NavLink to="/projects" className="navLink">
-              <p>Projects</p>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/aboutme" className="navLink">
-              <p>About me</p>
-            </NavLink>
-          </li>
-          <li>
-            <Link to="/contact">
-              <Button
-                className="contactButton"
-              >
-                Contact
-              </Button>
+    <>
+      <nav id="navDesktop" >
+        <div id="nav" className="navContainer">
+          <Link  className="navContainer__logoContainer" to="/">
+            <img src={logo} alt="Logo" />
+            <p id="navName">
+              Lucien Peltier <strong className="fira-mono-bold">|</strong>
+            </p>
+            <p className="fira-mono-bold" id="navJob">
+              UX/UI/Graphic designer
+              <br />& Front-end developer
+            </p>
+          </Link>
+          <ul>
+            <li>
+              <NavLink to="/projects" className="navLink">
+                <p>Projects</p>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/aboutme" className="navLink">
+                <p>About me</p>
+              </NavLink>
+            </li>
+            <li>
+              <Link to="/contact">
+                <Button className="contactButton">Contact</Button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <nav id="navMobile" ref={navMobileRef}>
+        <div id="nav" className="mobileNavContainer">
+          <div ref={logoContainerRef} className="mobileNavContainer__logoButton" >
+            <img
+              src={logo}
+              alt="Logo"
+              className="mobileNavContainer__logoButton--logo"
+            />
+          </div>
+          <div className="mobileNavContainer__navBar" ref={navBarRef}>
+            <Link className="mobileNavContainer__navBar__name" to="/">
+              <p>
+                Lucien Peltier <br />
+                <strong className="fira-mono-bold">
+                  UX/UI/Graphic designer
+                  <br />& Front-end developer
+                </strong>
+              </p>
             </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            <ul>
+              <li>
+                <NavLink to="/projects" className="navLink">
+                  <p>Projects</p>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/aboutme" className="navLink">
+                  <p>About me</p>
+                </NavLink>
+              </li>
+              <li>
+                <Link to="/contact">
+                  <Button className="contactButton">Contact</Button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
